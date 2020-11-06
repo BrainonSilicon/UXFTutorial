70f2e7201bcb48e2715e98a0abd032fa834c36f0
BrainonSilicon <ssmbatchelor@gmail.com>
2020-11-06 22:25:51 +0000

    Finished Part 4 of UXF Tutorial

---
+++ Assets/Scripts/StartPointState.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartPointState : MonoBehaviour
+{
+        // define 3 public variables - we can then assign their color values in the inspector.
+    public Color red;
+    public Color amber;
+    public Color green;
+
+    // reference to the material we want to change the color of.
+    Material material;
+
+    /// Awake is called when the script instance is being loaded.
+    void Awake()
+    {
+        // get the material that is used to render this object (via the MeshRenderer component)
+        material = GetComponent<MeshRenderer>().material;
+    }
+
+    IEnumerator Countdown()
+    {
+        yield return new WaitForSeconds(0.5f);
+        material.color = green;
+    }
+
+    /// OnTriggerEnter is called when the Collider 'other' enters the trigger.
+    void OnTriggerEnter(Collider other)
+    {
+        material.color = amber;
+        StartCoroutine(Countdown());
+    }
+
+    /// OnTriggerExit is called when the Collider 'other' has stopped touching the trigger.
+    void OnTriggerExit(Collider other)
+    {      
+        StopAllCoroutines();
+        material.color = red;        
+    }
+}
